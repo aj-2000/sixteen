4f910235c5c847362f20dab2b8b41bff3c01ee60
Ajay Sharma <97158497+aj-2000@users.noreply.github.com>
2023-09-02 13:40:06 +0530

    Update consts.js

--- src/data/consts.js
+++ src/data/consts.js
@@ -1,6 +1,6 @@
 export const server_url =
   process.env.NODE_ENV === "production"
-    ? "https://sixteenbackend-production.up.railway.app"
+    ? "https://sixmeent-backend.onrender.com"
     : "http://localhost:5000";
 export const peerConnectionConfig = {
   iceServers: [
